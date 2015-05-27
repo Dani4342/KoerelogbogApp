c6271b2e7c8301faceebc28c551742c49fb32433
Daniel <Danielzaunbrecher42@gmail.com>
2015-05-27 13:40:25 +0200

    Initial commit

---
+++ Entry.java
@@ -0,0 +1,12 @@
+package com.example.daniel.koerelogbogapp;
+
+import java.util.Date;
+
+/**
+ * Created by Daniel on 27-05-2015.
+ */
+public class Entry {
+
+    private Date entryDate;
+    private String LicensePlate, entryFrom, entryTo,
+}
